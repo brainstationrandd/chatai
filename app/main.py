94e1af80d4dee1ffb2e30eb2635ea6ca10385027
TAHMID37 <tahmid.rahman@brainstation-23.com>
2024-09-20 18:23:08 +0600

    Refactor qna endpoint to accept question payload

--- app/main.py
+++ app/main.py
@@ -1,9 +1,12 @@
 from fastapi import FastAPI
 from utils.qna import qna_bot
-
+from pydantic import BaseModel
 app = FastAPI()
 
+class QuestionPayload(BaseModel):
+    question: str
+
 @app.post("/qna")
-async def qna(question: str):
-    response = qna_bot(question)
+async def qna(payload: QuestionPayload):
+    response = qna_bot(payload.question)
     return response
